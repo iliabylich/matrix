91cc74c07337e24fca043a9e3c6b799d6804b74f
Ilya Bylich <ibylich@gmail.com>
2022-02-15 22:49:21 +0300

    Implement Matrix.parse

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -4,5 +4,51 @@
 #   + require it directly
 #   + prepend to $LOAD_PATH
 
+# Representation of a matrix
 class Matrix
+  class ParseError < StandardError; end
+
+  # Parses given string into an instance of Matrix
+  #
+  # @param input [String] string to parse
+  #
+  # @return [Matrix]
+  def self.parse(input)
+    rows = input.lines.map { |row| row.chomp.chars }
+
+    expected_cols = rows.first.length
+    if (invalid_row_idx = rows.find_index { |row| row.length != expected_cols })
+      raise ParseError, <<~MSG
+        expected row number #{invalid_row_idx} to have #{expected_cols} columns, \
+        got #{rows[invalid_row_idx].length}
+      MSG
+    end
+
+    new(rows)
+  end
+
+  def initialize(rows)
+    @rows = rows
+  end
+
+  # Returns total number of rows
+  #
+  # @return [Integer]
+  def rows
+    @rows.length
+  end
+
+  # Returns total number of columns
+  #
+  # @return [Integer]
+  def cols
+    @rows.first.length
+  end
+
+  # Returns an element of matrix at +row+ row, +col+ col
+  #
+  # @return [String]
+  def [](row, col)
+    @rows[row][col]
+  end
 end
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -1,14 +1,31 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 require 'matrix'
 
 RSpec.describe Matrix do
   describe '.parse' do
     context 'when given string does not represent a NxM matrix' do
-      it 'throws an error'
+      it 'throws an error' do
+        expect do
+          Matrix.parse("abc\nde")
+        end.to raise_error(Matrix::ParseError, /expected row number 1 to have 3 columns, got 2/)
+        expect do
+          Matrix.parse("abc\ndefg")
+        end.to raise_error(Matrix::ParseError, /expected row number 1 to have 3 columns, got 4/)
+      end
     end
 
     context 'when given string represents a valid matrix' do
-      it 'parses it to rows and columns'
+      it 'parses it to rows and columns' do
+        matrix = Matrix.parse("0011\n1110")
+
+        expect(matrix.rows).to eq(2)
+        expect(matrix.cols).to eq(4)
+
+        expect(matrix[0, 0]).to eq('0')
+        expect(matrix[1, 2]).to eq('1')
+      end
     end
   end
 
